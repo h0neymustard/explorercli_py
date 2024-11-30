642a901c9507076a2be04ba0cf3d345241cb5fc1
H0neyMustard <wntviifxuvtrldjktwzbiehr@gmail.com>
2024-11-30 13:27:40 +0100

    Added Several mode, docstring and now support link elements

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-An simple python packages to browse in your machine and return the path of one object (several soon)
+An simple python packages that use inquirer and allow you to browse in your machine and return the path of one object (several soon).
+The commands are in the docstring function.
--- explorer.py
+++ explorer.py
@@ -1,28 +1,92 @@
 import inquirer
 import os
 
-def explorer()->str:
-    while True:
-        wdir = os.getcwd()
-        print("\n"*5)
-        print(f"→ {wdir}\\")
-        choices = [f"🗀 {i}" if os.path.isdir(i) 
-                #    or os.path.islink(i)
-                    else f"🗎 {i}" for i in os.listdir()]
-        choices+=["🗀 .. (go up parent folder)"]
-        choices.sort()
-        selected = inquirer.list_input(
-                        "Select file in the current directory",
-                        choices=choices
-        )
-        if selected=="🗀 .. (go up parent folder)":
-            os.chdir("..")
-        else:
-            if os.path.isdir(selected[2:]):
-                os.chdir(selected[2:])
-            else:
-                file_path = os.path.join(wdir, selected[2:])
-                return file_path
 
-if __name__ == '__main__':
+def clear():
+    """
+    Just a func founded on internet to clear the terminal according to the right operating system
+    """
+    # for windows
+    if os.name == "nt":
+        os.system("cls")
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        os.system("clear")
+
+
+def explorer() -> str:
+    """
+    Simple function to select one of several file/folder, In single mode you can navigate easily with up,down, and enter to go in the folder selected
+    but if you choose an file the func gonna return his path.
+
+    Instead in "several mode" you can choose one or multiple file/folder at one time ,(right arrow to select),
+    in the current directory. 
+    And if you choose nothing it loop,
+    but if you have "<-> Return to navigation mode" in your selection its gonna just return in the single mode
+
+    Returns:
+        str: Return the path of the selected file/folder(s)
+    """
+    several_mode = False
+
+    while True:
+
+        # Display of current directory
+        wdir = os.getcwd()
+
+        clear()
+        print("\n" * 5)
+        print(f"→ {wdir}\\")
+        choices = []
+        for each in os.listdir():
+            if os.path.isdir(each):
+                choices.append(f"🗀 {each}")
+            elif os.path.islink(each):
+                choices.append(f"L {each}")
+            elif os.path.isfile(each):
+                choices.append(f"🗎 {each}")
+            else:
+                choices.append(f"? {each}")
+
+        # Single/Navigation mode
+        if not several_mode:
+            choices += ["↰ 🗀 .. (go to parent folder)"]
+            choices.sort()
+            choices.insert(1, "<-> Go to several mode")
+
+            selected = inquirer.list_input(
+                "Navigate in the current directory Select file in the current directory oror ",
+                choices=choices,
+            )
+            print(selected)
+            if selected == "↰ 🗀 .. (go to parent folder)":
+                os.chdir("..")
+            elif selected == "<-> Go to several mode":
+                several_mode = True
+            else:
+                if os.path.isdir(selected[2:]) or os.path.islink(selected[2:]):
+                    try:
+                        os.chdir(selected[2:])
+                    except PermissionError as e:
+                        print("Yout dont have the permissions to go here.\n>", e)
+                else:
+                    file_path = os.path.join(wdir, selected[2:])
+                    return file_path
+        # Several mode (that u can select folder in this mode)
+        else:
+            choices.sort()
+            choices.insert(0, "<-> Return to navigation mode")
+            selected = inquirer.checkbox(
+                "Select file in the current directory", choices=choices
+            )
+            if selected:
+                if "<-> Return to navigation mode" in selected:
+                    several_mode = False
+                else:
+                    return [os.path.join(wdir, i[2:]) for i in selected]
+            else:
+                pass
+
+
+if __name__ == "__main__":
     print(explorer())
